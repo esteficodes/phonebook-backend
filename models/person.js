e3b1a5dc1d9237d7a435cc99abd1f1a615ba9d24
Estefanía Q. Lusby <estefaniaquevedolusby@gmail.com>
2025-07-08 14:40:51 +0200

    last changes

--- models/person.js
+++ models/person.js
@@ -3,14 +3,30 @@ const mongoose = require('mongoose');
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 3,
-    required: true
+    minlength: 3,
+    required: [true, 'Name is required']
   },
   number: {
     type: String,
-    required: true
+    required: [true, 'Phone number is required'],
+    validate: {
+      validator: function (v) {
+        // Must be in format: 2 or 3 digits, dash, followed by digits, total length ≥ 8
+        return /^\d{2,3}-\d+$/.test(v) && v.length >= 8;
+      },
+      message: props => `${props.value} is not a valid phone number! It should be in the format XX-XXXXXXX or XXX-XXXXXXX.`
+    }
   }
-})
+});
 
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+});
 
 module.exports = mongoose.model('Person', personSchema);
+
+
